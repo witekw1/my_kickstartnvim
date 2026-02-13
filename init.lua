87a22bc22c066955cb00132ae906660552c66317
wito <witek.waligora@gmail.com>
2026-02-13 23:58:58 +0100

    mason fix

--- init.lua
+++ init.lua
@@ -481,7 +481,10 @@ require('lazy').setup({
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'mason-org/mason.nvim', opts = {} },
+      { 'mason-org/mason-lspconfig.nvim', opts = {} },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+      -- temporary fix
 
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
